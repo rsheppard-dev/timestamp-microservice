7433b7d1cfbf1c7e8a6ba083a66788ec51b816a9
rsheppard83 <rsheppard83@gmail.com>
2021-06-06 09:13:52 +0100

    first attempt

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // where your node app starts
 
 // init project
+const moment = require('moment-timezone')
 var express = require('express');
 var app = express();
 
@@ -9,6 +10,9 @@ var app = express();
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// setup port
+const port = process.env.PORT || 3000
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -24,9 +28,31 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// translate date
+app.get('/api/:date', (req, res) => {
+  const date = req.params.date.includes('-') ?
+    new Date(req.params.date) :
+    new Date(parseInt(req.params.date))
+  const timezone = moment.tz.guess()
+  const unix = Math.floor(date.getTime() / 1000)
+  const utc = moment(date).tz(timezone).format('ddd, DD MMM YYYY hh:mm:ss z')
+
+  // check if user entered valid date
+  if (!date.getTime()) {
+    return res.json({
+      error: 'Invalid Date'
+    })
+  }
+
+  res.json({
+    unix,
+    utc
+  })
+})
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(port, function () {
+  console.log('Your app is listening on port ' + port);
 });
